7755f7b185c009e6289b2cc9e306be1be2ac794c
tienhgm <47660593+tienhgm@users.noreply.github.com>
2020-11-14 11:47:34 +0700

    update detail product

--- Backend/Views/ProductsView.php
+++ Backend/Views/ProductsView.php
@@ -1,4 +1,4 @@
-<div class="col-md-12" style="margin-left: 20px;">
+<div class="col-md-12" >
     <div style="margin-bottom:5px;">
         <a href="index.php?controller=products&action=create" class="btn btn-primary">Add products</a>
     </div>
@@ -13,17 +13,18 @@
                   <th scope="col">Category</th>
                   <th scope="col">PRICE</th>
                   <th scope="col">ADDRESS</th>
-                  <th scope="col">AREA</th>
+                  <!-- <th scope="col">AREA</th> -->
                   <th scope="col">DISCOUNT</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Phòngtắm</th>
+                  <!-- <th scope="col">Description</th> -->
+                  <!-- <th scope="col">Phòngtắm</th>
                   <th scope="col">Phòngbếp</th>
                   <th scope="col">ChungChủ</th>
                   <th scope="col">Điềuhòa</th>
                   <th scope="col">Bancông</th>
-                  <th scope="col">Điện_Nước</th>
+                  <th scope="col">Điện_Nước</th> -->
                   <th scope="col">Quantities</th>
                   <!-- <th scope="col">DATE</th> -->
+                  <th scope="col">detail</th>
                   <th scope="col"></th>
                 </tr>
               </thead>
@@ -35,30 +36,30 @@
                   <th><?php echo $this->modelGetCategoryName($rows->category_id); ?></th>
                   <th> <?php echo number_format($rows->price);?> VNĐ</th>
                   <td><?php echo $rows->address; ?></td>
-                  <td><?php echo number_format($rows->area);?> m2</td>
+                 <!--  <td><?php echo number_format($rows->area);?> m2</td> -->
                   <td><?php echo number_format($rows->discount); ?> %</td>
-                  <td><?php echo $rows->description; ?></td>
+                  <!-- <td><?php echo $rows->description; ?></td>
                   <td><?php echo $rows->phong_tam;?></td>
                   <td><?php echo $rows->phong_bep;?></td>
                   <td style="text-align: center;"> 
-                  	<?php if($rows->chung_chu == 1): ?>
+                    <?php if($rows->chung_chu == 1): ?>
                         <span class="fa fa-check"></span>
                     <?php endif; ?>
                   </td>
                   
                   <td style="text-align: center;">
-                  	<?php if($rows->dieu_hoa == 1): ?>
+                    <?php if($rows->dieu_hoa == 1): ?>
                         <span class="fa fa-check"></span>
                     <?php endif; ?>
                   </td>
                   <td style="text-align: center;">
-                  	<?php if($rows->ban_cong == 1): ?>
+                    <?php if($rows->ban_cong == 1): ?>
                         <span class="fa fa-check"></span>
                     <?php endif; ?>
                   </td>
-                  <td><?php echo $rows->gia_dien_nuoc;?></td>
+                  <td><?php echo $rows->gia_dien_nuoc;?></td> -->
                   <td><?php echo $rows->quantities;?></td>
-                  <!-- <td></td> -->
+                  <td><a href="index.php?controller=products&action=detail&id=<?php echo $rows->id;?>" class="label label-success">Chi tiết</a></td>
                   <td style="text-align:center;">
                     <a href="index.php?controller=products&action=update&id=<?php echo $rows->id; ?>">Edit</a>&nbsp;
                     <a href="index.php?controller=products&action=delete&id=<?php echo $rows->id; ?>" onclick="return window.confirm('Are you sure?');">Delete</a>
